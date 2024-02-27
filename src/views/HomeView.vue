ba48dea02ed0416fc2ea0b7af19ba998704a0181
Leandro Melo <matheusleandro33@gmail.com>
2024-02-27 11:55:46 -0300

    feat: adding vuetify-list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,59 @@
 <script setup>
-
+  //let items = data: () => ({[{name: "oi"}, {name: "tchau"}]})
+  /*export default {
+    data: () => ({
+      items: [
+        {
+          title: 'Item #1',
+          value: 1,
+        },
+        {
+          title: 'Item #2',
+          value: 2,
+        },
+        {
+          title: 'Item #3',
+          value: 3,
+        },
+        {
+          title: 'Item #4',
+          value: 4,
+        },
+      ],
+    }),
+  }*/
 </script>
 
 <template>
-  <main class = "container">
-    
+  <main class="container">
     <h1>Start Saving your Decks</h1>
 
-    <div class = "container-decks">
+    <div class="container-decks">
+
       <p>Your deck list is empty</p>
-      <v-list lines="two">
-        <v-list-item
-          v-for="n in 3"
-          :key="n"
-          :title="'Item ' + n"
-          subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
-        ></v-list-item>
-      </v-list>            
+
+      <v-card max-width="1500" min-width="300">
+        <v-list lines="two">
+
+          <v-list-item
+            :items="items"
+            :key="n"
+            :title="'Item ' + n"
+            subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+          >
+          </v-list-item>
+
+        </v-list>      
+      </v-card>
+
+      <!-- 
+      <v-card max-width="1500" min-width="300">
+        <v-list :items="items"></v-list>
+      </v-card>
+      -->
     </div>
 
-    <v-btn prepend-icon="$vuetify">
-      Add Deck
-    </v-btn>
-
+    <v-btn prepend-icon="$vuetify"> Add Deck </v-btn>
   </main>
 </template>
 
@@ -30,19 +61,14 @@
 .container {
   display: flex;
   height: 100vh;
-  background-color: lightblue;
-  margin: 0px;
-  padding: 0px;
   justify-content: space-evenly;
   align-items: center;
   flex-direction: column;
 }
 .container-decks {
-  display:flex;
+  display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
 }
 </style>
-
-
