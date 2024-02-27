b69a680e7d6701006641a6ef12a6397e2d82cb03
Leandro Melo <matheusleandro33@gmail.com>
2024-02-27 09:49:53 -0300

    feat: start the vue project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+</script>
+
+<template>
+  <main class = "container">
+    
+    <h1>Start Saving your Decks</h1>
+
+    <div class = "container-decks">
+      <p>Your deck list is empty</p>
+      <v-list lines="two">
+        <v-list-item
+          v-for="n in 3"
+          :key="n"
+          :title="'Item ' + n"
+          subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+        ></v-list-item>
+      </v-list>            
+    </div>
+
+    <v-btn prepend-icon="$vuetify">
+      Add Deck
+    </v-btn>
+
+  </main>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  height: 100vh;
+  background-color: lightblue;
+  margin: 0px;
+  padding: 0px;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+}
+.container-decks {
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
+
