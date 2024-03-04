e4c087c9ddeeaa7e8534c3cb28b0188a543693b2
Matheus Leandro <matheusleandro33@gmail.com>
2024-03-04 10:07:42 -0300

    feat: add button and text field components

---
+++ crud-front/src/components/Button.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+    const props = defineProps<{
+        iconName: string;
+        buttonText: string;
+    }>();
+
+</script>
+
+<template>
+    <v-btn class="bg-blue-lighten-3" :prepend-icon="props.iconName"> {{ props.buttonText }} </v-btn>  
+</template>
---
+++ crud-front/src/components/InputText.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+    const props = defineProps<{
+        iconName: string;
+        label: string;
+    }>();
+    
+</script>
+
+<template>
+    <v-text-field variant="underlined" :label="props.label" clearable>
+    <template v-slot:prepend-inner>
+        <v-icon :icon="props.iconName"></v-icon>
+    </template>
+    </v-text-field>
+</template>
