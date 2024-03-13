e4fe34a9036fe4c9017f62ff5575b7536fe78c3b
Matheus Leandro <matheusleandro33@gmail.com>
2024-03-13 08:49:56 -0300

    refactor: remove unused imports

--- crud-back/src/main/java/study/crudback/model/Deck.java
+++ crud-back/src/main/java/study/crudback/model/Deck.java
@@ -3,16 +3,11 @@ package study.crudback.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 
